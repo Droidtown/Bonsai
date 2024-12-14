407d2bad0df297f08ee2d4030c15cd17aca087a8
Cathy <anching.cathy@gmail.com>
2024-12-14 23:56:08 +0800

    Update syntax.py

--- linguistics/syntax.py
+++ linguistics/syntax.py
@@ -14,6 +14,8 @@ class TransformationalGrammar:
         self.whoPAT = re.compile("<CLAUSE_whoQ>[^<]+</CLAU>")
         self.twPAT = {"who" :re.compile("(?<=<ENTITY_person>)[^<]+(?=</ENTITY_person><A[CU])|(?<=<ENTITY_pronoun>)[^<]+(?=</ENTITY_pronoun><A[CU])"),
                       "what":re.compile("((?<=<ENTITY_oov>)|(?<=<ENTITY_noun>)|(?<=<ENTITY_nouny>)|(?<=<ENTITY_nounHead>))[^<]+((?=</ENTITY_oov>(?!<RANGE))|(?=</ENTITY_noun>(?!<RANGE))|(?=</ENTITY_nouny>(?!<RANGE))|(?=</ENTITY_nounHead>(?!<RANGE)))"),
+                      "where":re.compile("((?=</FUNC_inner><LOCATION>).*)"),
+                      "yesno":re.compile("((?!=</ENTITY_pronoun><AUX>[^<+]</AUX>).*)")
                       }
         if online == False:
             self.url = ""
@@ -43,6 +45,8 @@ class TransformationalGrammar:
         resultDICT = self.articut.parse(self.inputSTR)
         self._whoQ(resultDICT)
         self._whatQ(resultDICT)
+        #self._whereQ(resultDICT)
+        self._yesnoQ(resultDICT)
         return self.QDICT
 
     def _whoQ(self, resultDICT):
@@ -70,9 +74,35 @@ class TransformationalGrammar:
                     self.QDICT["what"].append(self.purgePAT.sub("", self.twPAT["what"].sub("什麼", resultDICT["result_pos"][i], count=1))+"呢？")
         return None
 
+    #def _whereQ(self, resultDICT):
+        #self.whoQLSIT = []
+        #for i in range(0, len(resultDICT["result_pos"])):
+            #if len(resultDICT["result_pos"][i]) == 1:
+                #pass
+            #elif resultDICT["result_pos"][i] in self.QDICT["where"]:
+                #pass
+            #else:
+                #if self.lang == "tw":
+                    #print(resultDICT["result_pos"][i])
+                    #self.QDICT["where"].append(self.purgePAT.sub("", self.twPAT["where"].sub("哪裡", resultDICT["result_pos"][i], count=1)))
+        #return None
+    
+    def _yesnoQ(self, resultDICT):
+        self.whoQLSIT = []
+        for i in range(0, len(resultDICT["result_pos"])):
+            if len(resultDICT["result_pos"][i]) == 1:
+                pass
+            elif resultDICT["result_pos"][i] in self.QDICT["yesno"]:
+                pass
+            else:
+                if self.lang == "tw":
+                    print(resultDICT["result_pos"][i])
+                    self.QDICT["yesno"].append(self.purgePAT.sub("", self.twPAT["yesno"].sub(resultDICT["result_pos"][i], resultDICT["result_pos"][i], count=1))+"嗎？")
+        return None    
 
 if __name__ == "__main__":
     tg = TransformationalGrammar(lang="tw")
     resultDICT = tg.makeQ("川普出生並成長於紐約州紐約市皇后區，他是美國歷史上最富有的總統")
+    #resultDICT = tg._whatQ("川普出生並成長於紐約州紐約市皇后區，他是美國歷史上最富有的總統")
 
     pprint(resultDICT)
